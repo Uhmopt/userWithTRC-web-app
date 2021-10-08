5540adeefb6cdb34c45724554da72134b2a653fc
TDev0206 <enginechair1959@gmail.com>
2021-10-08 22:54:24 +0800

    [Update] footer button

--- src/components/CustomSvgs.js
+++ src/components/CustomSvgs.js
@@ -1,10 +1,16 @@
 import Home from 'assets/images/svgs/Home.svg'
+import Invite from 'assets/images/svgs/Invite.svg'
+import Revenue from 'assets/images/svgs/Revenue.svg'
+import Contact from 'assets/images/svgs/Contact.svg'
 import UpgradeUser from 'assets/images/svgs/UpgradeUser.svg'
 import React from 'react'
 
 const svgs = {
   UpgradeUser,
   Home,
+  Revenue,
+  Invite,
+  Contact
 }
 
 export default function CustomSvgs({ name = '', alt = '', ...props }) {
--- src/layouts/FooterBar.js
+++ src/layouts/FooterBar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 const menuItems = [
   { label: 'Home', icon: <CustomSvgs className="icon" label="Home" /> },
-  { label: 'Revenue', icon: '🔍' },
+  { label: 'Revenue', icon: <CustomSvgs name="Revenue" className="icon m-auto" label="Revenue" /> },
   { label: '', icon: '' },
-  { label: 'Invite firiend', icon: '🔔' },
-  { label: 'Contact', icon: '👱' },
+  { label: 'Invite friend', icon: <CustomSvgs name="Invite" className="icon m-auto" label="Invite" /> },
+  { label: 'Contact', icon: <CustomSvgs name="Contact" className="icon m-auto" label="Contact" /> },
 ]
 
 export default function FooterBar() {
@@ -18,7 +18,7 @@ export default function FooterBar() {
   return (
     <div className="footer-bar">
       <div className="both left"></div>
-      <nav style={{ '--k': selectedMenuIndex }}>
+      <nav className="m-auto" style={{ '--k': selectedMenuIndex }}>
         {menuItems.map((item, itemIndex) => (
           <Link
             key={itemIndex}
@@ -30,12 +30,11 @@ export default function FooterBar() {
             style={{ '--i': itemIndex }}
           >
             {item?.icon ?? ''}
-            <div className="label">{item?.label ?? ''}</div>
+            <div className="label ">{item?.label ?? ''}</div>
           </Link>
         ))}
       </nav>
       <div className="both right"></div>
-
       <div className="middle-button-container">
         <CustomSvgs name="UpgradeUser" />
       </div>
