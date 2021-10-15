3f73f719941da9645d1ef6e339a9f4166fa46d45
TDev0206 <enginechair1959@gmail.com>
2021-10-15 12:31:42 +0800

    [Fix] LevelAuthorityCard userlevelicon change

--- src/components/LevelAuthorityCard.js
+++ src/components/LevelAuthorityCard.js
@@ -2,15 +2,15 @@ import LockIcon from '@mui/icons-material/Lock';
 import { Card, CardActions } from '@mui/material';
 import React from 'react';
 
-export default function LevelAuthorityCard({ startIcon = '' }) {
+export default function LevelAuthorityCard({ startIcon = '', levelNum = 0 }) {
   return (
     <div className="pt-4">
       <Card className="bg-white rounded-md flex justify-between p-2">
         <div className="flex justify-between">
           {startIcon}
           <div className="self-center pl-2">
-            <div className="text-lg font-bold text-title">1 Star member</div>
-            <div className="text-xs">Unlock level 1 Friend</div>
+            <div className="text-lg font-bold text-title">{`${levelNum ?? 0} Star member`}</div>
+            <div className="text-xs">{`Unlock level ${levelNum ?? 0} Friend`}</div>
           </div>
         </div>
         
--- src/components/LevelAuthorityTable.js
+++ src/components/LevelAuthorityTable.js
@@ -3,49 +3,25 @@ import LevelAuthorityCard from './LevelAuthorityCard'
 import UserLevelIcon from './UserLevelIcon'
 
 export default function LevelAuthorityTable() {
+  const tmpData = new Array(16).fill(0)
   return (
     <div className="mt-6 mb-24">
       <label className="text-title text-lg">Level Authority</label>
-      <LevelAuthorityCard
-        startIcon={
-          <UserLevelIcon
-            levelNum="0"
-            alt="Star"
-            className="mx-auto w-10"
-            iconClass="user-level-icon-small"
+      {tmpData.map((item, index) => {
+        return (
+          <LevelAuthorityCard
+            startIcon={
+              <UserLevelIcon
+                levelNum={index}
+                alt="Star"
+                className="mx-auto w-10"
+                iconClass="user-level-icon-small"
+              />
+            }
+            levelNum = {index}
           />
-        }
-      />
-      <LevelAuthorityCard
-        startIcon={
-          <UserLevelIcon
-            levelNum="0"
-            alt="Star"
-            className="mx-auto w-10"
-            iconClass="user-level-icon-small"
-          />
-        }
-      />
-      <LevelAuthorityCard
-        startIcon={
-          <UserLevelIcon
-            levelNum="0"
-            alt="Star"
-            className="mx-auto w-10"
-            iconClass="user-level-icon-small"
-          />
-        }
-      />
-      <LevelAuthorityCard
-        startIcon={
-          <UserLevelIcon
-            levelNum="0"
-            alt="Star"
-            className="mx-auto w-10"
-            iconClass="user-level-icon-small"
-          />
-        }
-      />
+        )
+      })}
     </div>
   )
 }
